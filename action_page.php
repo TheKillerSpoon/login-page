4b5408a3181a358f4b31d49ad82bb9528115bb24
TheKillerSpoon <75366115+TheKillerSpoon@users.noreply.github.com>
2024-04-03 15:42:16 +0200

    yes

---
+++ action_page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE php>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>info</title>
+</head>
+<body>
+    Your username is: <?php echo $_GET["uname"]; ?><br>
+    Your Password is: <?php echo $_GET["psw"]; ?>        
+</body>
+</html>
